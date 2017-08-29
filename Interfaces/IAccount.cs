11e68ed2a7bf66e25d3750530e2bf6b3360b0b28
Firefly4266 <gaines.johnp@gmail.com>
2017-08-28 23:11:14 -0700

    Minor correction

--- Interfaces/IAccount.cs
+++ Interfaces/IAccount.cs
@@ -6,12 +6,30 @@ using System.Threading.Tasks;
 
 namespace Interfaces
 {
-    interface IAccount
+    //interface IAccount
+    //{
+    //    void Deposit(decimal amount);
+    //    void Withdraw(decimal amount);
+    //    decimal Balance { get; }
+    //    string Owner { get; set; }
+    //    int Id { get; }
+    //}
+    interface IBasicAccount
     {
         void Deposit(decimal amount);
         void Withdraw(decimal amount);
         decimal Balance { get; }
+    }
+    interface IAccountInfo
+    {
         string Owner { get; set; }
         int Id { get; }
+    }
+    interface IAccount : IBasicAccount, IAccountInfo
+    {
+    }
+    interface IAccount2 : IAccount
+    {
+        void NewMethod();
     }
 }
