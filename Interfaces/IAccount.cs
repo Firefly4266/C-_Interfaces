30b9a214ece5bfe0879f8ee6c934305fb601d052
Firefly4266 <gaines.johnp@gmail.com>
2017-08-28 22:32:54 -0700

    coding out examples

---
+++ Interfaces/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IAccount
+    {
+        void Deposit(decimal amount);
+        void Withdraw(decimal amount);
+        decimal Balance { get; }
+        string Owner { get; set; }
+        int Id { get; }
+    }
+}
