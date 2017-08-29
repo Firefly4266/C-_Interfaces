12b5c5863d143bab69d1b6cf103310c7b77cdf19
Firefly4266 <gaines.johnp@gmail.com>
2017-08-29 00:29:21 -0700

    new implementing interfaces branch with example code.

---
+++ Interfaces/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+interface IBasicAccount
+{
+    void Deposit(decimal amount);
+    void Withdraw(decimal amount);
+    decimal Balance { get; }
+}
+public class Account : IBasicAccount
+{
+    private decimal balance;
+    public Account(decimal balance)
+    {
+        this.balance = balance;
+    }
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+    }
+    public void Withdraw(decimal amount)
+    {
+        balance -= amount;
+    }
+    public decimal Balance
+    {
+        get
+        {
+            return balance;
+        }
+    }
+}
